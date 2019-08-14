7b7d0acccc3dd3b35461d191a4a2e858e9d04d12
Josep <you@gmail.com>
2019-08-14 14:44:09 +0200

    init express-app

--- staff/josep-boix/express-app/index.js
+++ staff/josep-boix/express-app/index.js
@@ -1,7 +1,10 @@
-const express = require ('express')
-const http = require ('http')
+//conseguimos enviar fragmentos de template HTML
 
-const {argv:[,,port]}= process
+const http = require ('http')
+const express = require ('express')
+
+const {argv:[,,port]} = process
+
 const app = express() // llama servidor
 
 app.get ('/', (req, res)=> {
@@ -9,34 +12,61 @@ app.get ('/', (req, res)=> {
         <input type="text" name="q">
         <button>Search</button>
         </form>`)
-}).listen (port)
-
-app.get('/pepito', (req, res)=>{
-    res.send(`<div>
-            <h3>pepito<h3>
-            <a href='/'>search</a>
-        </div>`
-    )
 })
+
+// app.get('/pepito', (req, res)=>{
+//     res.send(`<div>
+//             <h3>pepito<h3>
+//             <a href='/'>search</a>
+//         </div>`
+//     )
+// })
+
 
 app.get ('/search', (req, res)=> {
-    debugger
+    let content=''
     const {query: {q}}= req
-    const url = 
-    const request= http.get(url, reposnse =>{
-        let content=''
-        Response.on ('error', error=> {throw new Error(error)})
-        Response.on ('data', chunk=> content += chunk)
-        Response.on ('end', ()=> {
+    debugger
+
+    http.get(`http://duckling-api.herokuapp.com/api/search?q=${q}`, response => {
+        response.on ('error', error=> {throw new Error(error)})
+        response.on ('data', chunk=> content += chunk)
+        response.on ('end', ()=> {
             const jsonData = JSON.parse(content)
             const ducks = jsonData.map(duck =>{
-                const {title, imageUrl, price} = duck
+                const {title, imageUrl, price, id} = duck
                 return `<li>
-                    <h2>title<h2>
-                    <img>
-                    <span>
-                </li>`
+                <a href='/ducks/${id}'>
+                        <h2>${title}<h2>
+                        <img src="${imageUrl}">
+                        <span>${price}</span>
+                        </a>
+                        </li>`
+        })
+        res.send(`<ul>${ducks.join('')}</ul>`)
+        })
+    }).on('error', error => console.log(error))
+})
+
+app.get('/ducks/:id', (req, res) =>{
+    const {params: {id}} = req
+    let content = ''
+    http.get(`http://duckling-api.herokuapp.com/api/ducks/${id}`, response => {
+        response.on ('error', error=> {throw new Error(error)})
+        response.on ('data', chunk=> content += chunk)
+        response.on ('end', ()=> {
+            const duck = JSON.parse(content)
+                const {title, imageUrl, price, description, link} = duck
+                const htmlResponse = `<article>
+                    <h2>${title}<h2>
+                    <img src="${imageUrl}">
+                    <span>${price}</span>
+                    <p>${description}</p>
+                    <a href="${link}" target= "_blank">Go To Store</a>
+                </article>`
+                res.send(htmlResponse)
             })
         })
-    })
 })
+
+app.listen(port)
